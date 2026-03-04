caaaffa25bd8fc408e8276e6a3df20548a32a63f
Aloka_Dev <alokachamod3@gmail.com>
2026-03-04 15:43:40 +0530

    Mask database password in connection.php
    Updated database connection credentials to hide sensitive information.

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@ class Database{
     public static function setUpConnection(){
 
         if(!isset(Database::$connection)){
-            Database::$connection = new mysqli("localhost","root","Chamopathi.1","flydolk","3306");
+            Database::$connection = new mysqli("localhost","root","**********","flydolk","3306");
         }
     }
 
